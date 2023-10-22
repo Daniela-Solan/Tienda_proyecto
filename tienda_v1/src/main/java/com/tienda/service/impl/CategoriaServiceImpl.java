aa9760e5c8350f7aa45ab1cc0833109846cd6d05
Daniela-Solano <daniisofia9@hotmail.com>
2023-10-22 16:28:44 -0600

    Semana 6

--- tienda_v1/src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ tienda_v1/src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -6,20 +6,39 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
 
-    @Autowired // La anotacion autowired crea un unico objeto mientras se ejecuta la aplicacion
+    @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Categoria> getCategorias(boolean activos) {
-        var lista = categoriaDao.findAll(); //encontrar todos los datos quw tenga la lista
-
+        var lista = categoriaDao.findAll();
         if (activos) {
-            lista.removeIf(e -> !e.isActivo()); //Esta expresion nos permite saber si el objeto esta activo o no
+            lista.removeIf(e -> !e.isActivo());
         }
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 }
